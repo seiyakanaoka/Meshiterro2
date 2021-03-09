59bf515ac8a48287f2155f0793b94df3b4493f4c
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-03-09 14:21:56 +0000

    [Update]Meshiterro2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b4afa937472ea8cf9ca65b90c4ebc2feaf0dba533f21ce37ed3be15bbe43700e329378ce1445005379d63b3163019244e0082b67891e1ace51a12c2f80004cf0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  root 'homes#top'
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+   resource :favorite, only:[:create, :destroy]
+   
+   resources :post_comments, only:[:create, :destroy]
+  end
+  
 end
