667d08c8b0ce325eb5a6c5eafba5980040fa1b13
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-03-10 14:20:51 +0000

    [Update]Meshiterro2

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,10 +1,10 @@
 class PostCommentsController < ApplicationController
   def create
-    post_image = PostImage.find[params[:post_image_id]]
+    post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
-    redirect_to post_image_path(post_image_id)
+    redirect_to post_image_path(post_image)
   end
   
   def destroy
@@ -14,7 +14,7 @@ class PostCommentsController < ApplicationController
   
   private
   
-  def post_comment_paraqms
+  def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
-   resource :favorite, only:[:create, :destroy]
+   resource :favorites, only:[:create, :destroy]
    
    resources :post_comments, only:[:create, :destroy]
   end
+  resources :users, only:[:show, :edit, :update]
   
 end
