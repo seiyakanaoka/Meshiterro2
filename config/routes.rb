2ecec4e3c144f2326d78292c86fd44fe4bac2014
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-03-11 01:54:04 +0000

    [Update]Meshiterro2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root 'homes#top'
+  get 'homes/about'
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
    resource :favorites, only:[:create, :destroy]
    
