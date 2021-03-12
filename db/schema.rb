795e33f795bb408b6cf5bf981409bf6bc833f6dd
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-03-12 01:16:34 +0000

    [Update]Meshiterro2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_011855) do
+ActiveRecord::Schema.define(version: 2021_03_12_002108) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
